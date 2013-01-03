7ccd2e632515696e853cce5c1203e9d0bbaed2e3
Jörg Weller <weller@flagbit.de>
2013-01-03 17:08:09 +0100

    [MAGEMEP-64] better encoding handling

--- src/app/code/community/Flagbit/MEP/Model/Export/Adapter/Twig.php
+++ src/app/code/community/Flagbit/MEP/Model/Export/Adapter/Twig.php
@@ -213,6 +213,10 @@ class Flagbit_MEP_Model_Export_Adapter_Twig extends Mage_ImportExport_Model_Expo
 
     public function cleanLine($element)
     {
-        return trim(preg_replace('/\s\s+/', ' ', (html_entity_decode($element))));
+        return utf8_encode(
+                trim(
+                    preg_replace('/\s\s+/', ' ', html_entity_decode(htmlentities($element, ENT_SUBSTITUTE, 'UTF-8', false)))
+                    )
+                );
     }
 }
