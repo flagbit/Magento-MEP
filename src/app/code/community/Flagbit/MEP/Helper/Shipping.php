fec3312a5b405ef52d5679d8d0dd09c76d5814fa
Jörg Weller <weller@flagbit.de>
2013-03-05 17:20:16 +0100

    [BUGFIX] fixed cli run script error

--- src/app/code/community/Flagbit/MEP/Helper/Shipping.php
+++ src/app/code/community/Flagbit/MEP/Helper/Shipping.php
@@ -14,10 +14,10 @@
 class Flagbit_MEP_Helper_Shipping extends Mage_Core_Helper_Abstract
 {
 
-    private $_groupId = '0';
-    private $_storeId = '0';
-    private $_websiteId = '0';
-    private $orderData = array();
+    protected $_groupId = '0';
+    protected $_storeId = '0';
+    protected $_websiteId = '0';
+    protected $orderData = array();
 
     protected $_quote = null;
     protected $_shippingRequest = null;
@@ -76,6 +76,7 @@ class Flagbit_MEP_Helper_Shipping extends Mage_Core_Helper_Abstract
         return $this->_store[$storeId];
     }
 
+
     /**
      * calculate Shippingcosts
      *
@@ -84,6 +85,27 @@ class Flagbit_MEP_Helper_Shipping extends Mage_Core_Helper_Abstract
      * @param $profile Flagbit_MEP_Model_Profile
      */
     public function emulateCheckout($product, $store_id, $profile)
+    {
+        switch($profile->getCheckoutType()){
+
+            case 'quote':
+                return $this->emulateWithQuoteCheckout($product, $store_id, $profile);
+                break;
+
+            default:
+                return $this->emulateByShippingRequestCheckout($product, $store_id, $profile);
+                break;
+        }
+    }
+
+    /**
+     * calculate Shippingcosts
+     *
+     * @param $product Mage_Catalog_Model_Product
+     * @param $store_id int
+     * @param $profile Flagbit_MEP_Model_Profile
+     */
+    public function emulateByShippingRequestCheckout($product, $store_id, $profile)
     {
         $store = $this->_getStore($store_id);
         $productPrice = $product->getFinalPrice();
@@ -155,4 +177,189 @@ class Flagbit_MEP_Helper_Shipping extends Mage_Core_Helper_Abstract
         }
         return 0;
     }
+
+    public function emulateWithQuoteCheckout($item, $store_id, $profile)
+     {
+         $this->_product = $item;
+         $this->_storeId = $store_id;
+         $this->_websiteId = Mage::getModel('core/store')->load($store_id)->getWebsiteId();
+
+         $this->setOrderInfo($profile->getPaymentMethod(), $profile->getCountry(), $profile->getShippingMethod());
+         $orderData = $this->orderData;
+         if (!empty($orderData)) {
+             $this->_initSession($orderData['session']);
+             $quote = $this->_getOrderCreateModel()->getQuote();
+             $address = $quote->getShippingAddress();
+
+             $this->_getOrderCreateModel()->resetShippingMethod();
+             Mage::unregister('rule_data');
+             try {
+                 $this->_processQuote($orderData);
+                 if (!empty($orderData['payment'])) {
+                     $this->_getOrderCreateModel()->setPaymentData($orderData['payment']);
+                     $this->_getOrderCreateModel()->getQuote()->getPayment()->addData($orderData['payment']);
+                 }
+
+                 $address = $quote->getShippingAddress();
+
+                 $address->collectShippingRates();
+                 $rate = $address->getShippingRateByCode($profile->getShippingMethod());
+
+                 Mage::dispatchEvent('mep_calculate_shipping_rate', array(
+                     'product' => $this->_product, // Mage_Catalog_Model_Product
+                     'quote' => $quote, // Mage_Sales_Model_Quote
+                     'rate' => $rate // Mage_Sales_Model_Quote_Address_Rate
+                     )
+                 );
+
+                 //$_order = $this->_getOrderCreateModel()
+                 //    ->importPostData($orderData['order'])
+                 //    ->createOrder();
+                 $this->_getSession()->clear();
+                 Mage::unregister('rule_data');
+                 $quote->removeAllAddresses();
+                 if ($rate == false) return false;
+                 return $rate->getPrice();
+             } catch (Exception $e) {
+                 Mage::log("Order save error...".$e->getMessage());
+             }
+         }
+     }
+
+
+     public function setOrderInfo($payment_method, $country, $shipping_method)
+     {
+         $this->orderData = array(
+             'session' => array(
+                 'customer_id' => 0,
+                 'store_id' => $this->_storeId,
+             ),
+             'payment' => array(
+                 'method' => $payment_method,
+             ),
+             'add_products' => array(
+                 $this->_product->getId() => array('qty' => 1),
+             ),
+             'order' => array(
+                 'currency' => 'USD',
+                 'account' => array(
+                     'group_id' => $this->_groupId,
+                     'email' => 'test@test.de'
+                 ),
+                 'billing_address' => array(
+                     'prefix' => '',
+                     'firstname' => 'Max',
+                     'middlename' => '',
+                     'lastname' => 'Mustermann',
+                     'suffix' => '',
+                     'company' => '',
+                     'street' => array('Musterstrasse', ''),
+                     'city' => 'Musterhausen',
+                     'country_id' => $country,
+                     'region' => '',
+                     'region_id' => '',
+                     'postcode' => '12345',
+                     'telephone' => '012346789',
+                     'fax' => '',
+                 ),
+                 'shipping_address' => array(
+                     'prefix' => '',
+                     'firstname' => 'Max',
+                     'middlename' => '',
+                     'lastname' => 'Mustermann',
+                     'suffix' => '',
+                     'company' => '',
+                     'street' => array('Musterstrasse', ''),
+                     'city' => 'Musterhausen',
+                     'country_id' => $country,
+                     'region' => '',
+                     'region_id' => '',
+                     'postcode' => '12345',
+                     'telephone' => '012346789',
+                     'fax' => '',
+                 ),
+                 'shipping_method' => $shipping_method,
+                 'comment' => array(
+                     'customer_note' => 'This order has been programmatically created via import script.',
+                 )
+
+             ),
+         );
+     }
+
+     /**
+      * Retrieve order create model
+      *
+      * @return  Mage_Adminhtml_Model_Sales_Order_Create
+      */
+     protected function _getOrderCreateModel()
+     {
+         return Mage::getSingleton('adminhtml/sales_order_create');
+     }
+
+     /**
+      * Retrieve session object
+      *
+      * @return Mage_Adminhtml_Model_Session_Quote
+      */
+     protected function _getSession()
+     {
+         return Mage::getSingleton('adminhtml/session_quote');
+     }
+
+     /**
+      * Initialize order creation session data
+      *
+      * @param array $data
+      * @return Mage_Adminhtml_Sales_Order_CreateController
+      */
+     protected function _initSession($data)
+     {
+         /* Get/identify customer */
+         if (!empty($data['customer_id'])) {
+             $this->_getSession()->setCustomerId((int)$data['customer_id']);
+         }
+         /* Get/identify store */
+         if (!empty($data['store_id'])) {
+             $this->_getSession()->setStoreId((int)$data['store_id']);
+
+         }
+         return $this;
+     }
+
+     protected function _processQuote($data = array())
+     {
+         /* Saving order data */
+         if (!empty($data['order'])) {
+             $this->_getOrderCreateModel()->importPostData($data['order']);
+         }
+         $this->_getOrderCreateModel()->getQuote()->setCustomerIsGuest(1);
+
+         $this->_getOrderCreateModel()->getQuote()->setWebsiteId($this->_websiteId);
+         $this->_getOrderCreateModel()->getQuote()->setStoreId($this->_storeId);
+
+
+         $this->_getOrderCreateModel()->getBillingAddress();
+         $this->_getOrderCreateModel()->setShippingAsBilling(true);
+         /* Just like adding products from Magento admin grid */
+         if (!empty($data['add_products'])) {
+             $this->_getOrderCreateModel()->addProducts($data['add_products']);
+         }
+
+         /* Add payment data */
+         if (!empty($data['payment'])) {
+             $this->_getOrderCreateModel()->getQuote()->getPayment()->addData($data['payment']);
+         }
+         $this->_getOrderCreateModel()
+             ->initRuleData()
+         ->saveQuote();
+
+         //$this->_getOrderCreateModel()->getQuote()->save();
+
+         /* Collect shipping rates */
+         $this->_getOrderCreateModel()->collectShippingRates();
+
+         return $this;
+     }
+
 }
--- src/app/code/community/Flagbit/MEP/Model/Observer.php
+++ src/app/code/community/Flagbit/MEP/Model/Observer.php
@@ -6,17 +6,43 @@ class Flagbit_MEP_Model_Observer extends Varien_Object
      */
     public function exportEnabledProfiles()
     {
-        /** @var $profile Flagbit_MEP_Model_Profil */
+        /** @var $profile Flagbit_MEP_Model_Profile */
         foreach ($this->getProfileCollection() as $profile) {
+            $this->exportProfile($profile);
+        }
+    }
+
+    /**
+     * export Profile
+     *
+     * @param Flagbit_MEP_Model_Profile $profile
+     */
+    public function exportProfile(Flagbit_MEP_Model_Profile $profile)
+    {
+        $exportFile = null;
+        try{
+            /** @var $appEmulation Mage_Core_Model_App_Emulation */
+            $appEmulation = Mage::getSingleton('core/app_emulation');
+            //Start environment emulation of the specified store
+            $initialEnvironmentInfo = $appEmulation->startEnvironmentEmulation($profile->getStoreId());
+
+            // disable flat Tables
+            Mage::app()->getConfig()->setNode('catalog/frontend/flat_catalog_product',0,true);
+
             /* @var $export Mage_ImportExport_Model_Export */
             $export = Mage::getModel('mep/export');
             $export->setData('id', $profile->getId());
             $export->setEntity("catalog_product");
             $export->setFileFormat("twig");
             $export->setExportFilter(array());
-            $exportFile = $this->getExportPath($profile) . DS . $profile->getFilename();
+            $exportFile = $this->_getExportPath($profile) . DS . $profile->getFilename();
             file_put_contents($exportFile, $export->export());
+
+            $appEmulation->stopEnvironmentEmulation($initialEnvironmentInfo);
+        }catch (Exception $e){
+            Mage::logException($e);
         }
+        return $exportFile;
     }
 
     /**
@@ -24,7 +50,7 @@ class Flagbit_MEP_Model_Observer extends Varien_Object
      *
      * @return Flagbit_MEP_Model_Mysql4_Profile_Collection
      */
-    protected function getProfileCollection()
+    public function getProfileCollection()
     {
         /* @var $profiles Flagbit_MEP_Model_Mysql4_Profile_Collection */
         $profiles = Mage::getModel('mep/profile')->getCollection();
@@ -38,7 +64,7 @@ class Flagbit_MEP_Model_Observer extends Varien_Object
      * @param $profile Flagbit_MEP_Model_Profil
      * @return string
      */
-    protected function getExportPath($profile)
+    protected function _getExportPath($profile)
     {
         $exportDir = Mage::getConfig()->getOptions()->getBaseDir() . DS . $profile->getFilepath();
         Mage::getConfig()->getOptions()->createDirIfNotExists($exportDir);
