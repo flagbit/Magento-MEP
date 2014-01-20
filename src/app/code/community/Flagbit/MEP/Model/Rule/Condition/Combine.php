c07e6ae87d65d0d4b976cfc3299e2dc91a39ded2
pierre.bernard <pierre.bernard@flagbit.de>
2014-01-20 13:31:58 +0100

    [MAGEMEP-125] Adding child category filter support

--- src/app/code/community/Flagbit/MEP/Model/Rule/Condition/Combine.php
+++ src/app/code/community/Flagbit/MEP/Model/Rule/Condition/Combine.php
@@ -10,6 +10,7 @@ class Flagbit_MEP_Model_Rule_Condition_Combine
     public function __construct()
     {
         parent::__construct();
+        $this->setType('mep/rule_condition_combine');
     }
 
 
@@ -19,12 +20,7 @@ class Flagbit_MEP_Model_Rule_Condition_Combine
         $productAttributes = $productCondition->loadAttributeOptions()->getAttributeOption();
         $attributes = array();
         foreach ($productAttributes as $code=>$label) {
-            if ($code != 'category_ids') {
-                $attributes[] = array('value'=>'mep/rule_condition_product|'.$code, 'label'=>$label);
-            }
-            else {
-                $attributes[] = array('value'=>'catalogrule/rule_condition_product|'.$code, 'label'=>$label);
-            }
+            $attributes[] = array('value'=>'mep/rule_condition_product|'.$code, 'label'=>$label);
         }
         return $attributes;
     }
