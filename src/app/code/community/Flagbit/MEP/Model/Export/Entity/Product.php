c466aa33c640cb4464f136f190cdc452f8dda7eb
marlongehringer <marlon.gehringer@flagbit.de>
2015-04-28 13:06:10 +0200

    [SANETTA-528] fixed getting attribute without inheritance on single thread processing

--- src/app/code/community/Flagbit/MEP/Model/Export/Entity/Product.php
+++ src/app/code/community/Flagbit/MEP/Model/Export/Entity/Product.php
@@ -727,6 +727,12 @@ class Flagbit_MEP_Model_Export_Entity_Product extends Mage_ImportExport_Model_Ex
 			'_type' => '_getType'
         );
         $attrValue = $item->getData($attrCode);
+
+        if(empty($attrValue)) {
+            $item->load($item->getId());
+            $attrValue = $item->getData($attrCode);
+        }
+
         if (isset($attributeValueFilter[$attrCode])) {
             $attrValue = $this->$attributeValueFilter[$attrCode]($item, $mapItem);
         }
